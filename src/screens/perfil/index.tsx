570716279d74fb0c6bcf1b1822942a8a94acb5d9
AdryanDev7 <adryanlimarj@gmail.com>
2025-11-24 19:29:47 -0300

    feat: tela de perfil com foto

---
+++ src/screens/perfil/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { View, Text, Image, ScrollView } from 'react-native';
+
+const usuario = {
+    nome: "Peter Parker",
+    email: "peterparker@monetto.com",
+    foto: require("../../assets/petertrabalho.jpg"),
+    frase: "Aprenda a usar seu dinheiro de forma eficiente"
+};
+
+export const perfil = () => {
+    return (
+        <View>
+            <ScrollView>
+                <Text>Meu Perfil</Text>
+                <View>
+                    <View>
+                        <Image
+                            source={usuario.foto}
+                        />
+                    </View>
+                    <Text>{usuario.nome}</Text>
+                    <Text>{usuario.email}</Text>
+                    <View>
+                        <Text>
+                            "{usuario.frase}"
+                        </Text>
+                    </View>
+                </View>
+
+                <View>
+                    <Text>Configurações da Conta</Text>
+
+                    <View>
+                        <Text>Dados Pessoais</Text>
+                        <Text>{'>'}</Text>
+                    </View>
+
+                    <View>
+                        <Text>Segurança</Text>
+                        <Text>{'>'}</Text>
+                    </View>
+
+                    <View>
+                        <Text>Sobre o App</Text>
+                        <Text>{'>'}</Text>
+                    </View>
+                </View>
+
+            </ScrollView>
+        </View>
+    );
+};
